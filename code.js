817292aab031e054b6438248613541d7e694efba
Caden McFate <cadenmcfate@gmail.com>
2024-02-15 23:12:23 -0700

    implimented pancake sort

--- code.js
+++ code.js
@@ -1,8 +1,18 @@
 function flip(array, n) {
-    return array;
+    return array.slice(0,n).reverse().concat(array.slice(n));
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    for (let i = 0; i < array.length; i++) {
+	let maxIndex = i;
+	for (let j = i; j < array.length; j++) {
+	    if (array[j] > array[maxIndex]) maxIndex = j;
+	}
+	//Moving the maximum unsorted element to the front of the array:
+	array = flip(array, maxIndex);
+	array = flip(array, maxIndex+1);
+    }
     return array;
 }
+
